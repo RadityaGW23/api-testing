9c780c65f482eb1c910febe02890fd96d0c32cac
RadityaGW23 <radityag40@gmail.com>
2024-05-27 14:10:18 +0700

    Testing API

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,7 +1,4 @@
 exports.register = (req, res, next) => {
-    // const id = req.body.id;
-    // const uuid = req.body.uuid;
-    // const id_rfid = req.body.id_rfid;
     const name = req.body.name;
     const qty = req.body.qty;
     const location = req.body.location;
@@ -9,9 +6,6 @@ exports.register = (req, res, next) => {
     const result = {
         message: 'Register Success',
         data: {
-            // id: id,
-            // uuid: uuid,
-            // id_rfid: id_rfid,
             name: name,
             qty: qty,
             location: location
